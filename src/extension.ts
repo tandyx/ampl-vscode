969b5facfa59cc2cacb4bf0411543dd02b1d5394
Johan Cho <jcho@hks.harvard.edu>
2024-01-31 18:07:47 -0500

    push of current progress

--- src/extension.ts
+++ src/extension.ts
@@ -5,11 +5,11 @@
 import * as vscode from "vscode";
 import * as path from "path";
 import * as fs from "fs";
-import * as keyword from "./keyword";
+import * as _keyword from "./keyword";
 
 let g_terminal: vscode.Terminal;
 let terminal_open: boolean = false;
-const keywords: keyword.Keyword[] = JSON.parse(
+const keywords: _keyword.Keyword[] = JSON.parse(
   fs.readFileSync(
     path.join(__dirname, "..", "resources", "keywords.json"),
     "utf-8"
@@ -41,36 +41,69 @@ export function activate(context: vscode.ExtensionContext) {
     ): vscode.ProviderResult<vscode.Hover> {
       const word = document.getText(document.getWordRangeAtPosition(position));
       const keyword = keywords.find((k) => k.name === word);
-      if (!keyword || !keyword.description) {
-        return;
-      }
-      const markdownString = new vscode.MarkdownString();
-      markdownString.appendMarkdown(`### ${keyword.name}\n`);
-
-      if (keyword.type === "constant" && keyword.datatype) {
-        markdownString.appendMarkdown(`(${keyword.datatype}): `);
-      }
-
-      markdownString.appendMarkdown(keyword.description);
-      markdownString.appendText("\n");
-
-      if (keyword.type === "function" && keyword.parameters) {
-        markdownString.appendMarkdown("**Arguments**:\n");
-        for (const param of keyword.parameters) {
-          markdownString.appendMarkdown(
-            `- \`${param.name}\` (${param.type}): ${param.description}\n`
-          );
-        }
-        markdownString.appendMarkdown(`\n**returns**: ${keyword.datatype}\n`);
-      }
-
-      if (keyword.example) {
-        markdownString.appendCodeblock(keyword.example, "ampl");
-      }
-
+      if (!keyword || !keyword.description) return;
+      const markdownString = getKeywordMarkdown(keyword);
       return new vscode.Hover(markdownString);
     },
   });
+
+  vscode.languages.registerCompletionItemProvider("ampl", {
+    provideCompletionItems(document, position, token, context) {
+      const completionItems: vscode.CompletionItem[] = [];
+      for (const keyword of keywords) {
+        let completionItem;
+        switch (keyword.type) {
+          case "function":
+            completionItem = new vscode.CompletionItem(
+              keyword.name,
+              vscode.CompletionItemKind.Function
+            );
+          case "constant":
+            completionItem = new vscode.CompletionItem(
+              keyword.name,
+              vscode.CompletionItemKind.Constant
+            );
+          case "declaration":
+            completionItem = new vscode.CompletionItem(
+              keyword.name,
+              vscode.CompletionItemKind.Struct
+            );
+          default:
+            completionItem = new vscode.CompletionItem(
+              keyword.name,
+              vscode.CompletionItemKind.Keyword
+            );
+        }
+        completionItem.documentation = keyword.description;
+        completionItem.insertText = keyword.name;
+        completionItems.push(completionItem);
+      }
+      return completionItems;
+    },
+  });
+}
+
+function getKeywordMarkdown(keyword: _keyword.Keyword): vscode.MarkdownString {
+  const markdownString = new vscode.MarkdownString();
+  markdownString.appendMarkdown(`### ${keyword.name}\n`);
+  if (keyword.type === "constant" && keyword.datatype) {
+    markdownString.appendMarkdown(`(${keyword.datatype}): `);
+  }
+  markdownString.appendMarkdown(keyword.description);
+  markdownString.appendText("\n");
+  if (keyword.type === "function" && keyword.parameters) {
+    markdownString.appendMarkdown("**Arguments**:\n");
+    for (const param of keyword.parameters) {
+      markdownString.appendMarkdown(
+        `- \`${param.name}\` (${param.type}): ${param.description}\n`
+      );
+    }
+    markdownString.appendMarkdown(`\n**returns**: ${keyword.datatype}\n`);
+  }
+  if (keyword.example) {
+    markdownString.appendCodeblock(keyword.example, "ampl");
+  }
+  return markdownString;
 }
 
 function variableHover(
